eb2e038edc23cc841c8c3dc862f6161c5ad30a35
andrewc <andrewc@ICT203-T>
2024-10-07 10:26:25 +0200

    Add project files.

---
+++ ArrayGrowth/ArrayBasedVector.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayGrowth
+{
+    /// <summary>
+    /// A vector is an ordered (not sorted!) group of items
+    /// The items can be accessed by the "Rank": The Rank is the number of items before the current item.
+    /// In the case of the ArrayBasedVector, the items are stored in an array
+    /// The "Rank" is equal to the position in the array
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class ArrayBasedVector<T>
+    {
+        // Start off with an array of length 1 (mostly to be the same as the analysis on the slides)
+        // Ideally, you start off with a larger array
+        private const int DEFAULT_LENGTH = 8;
+        
+        // The array of items that are stored with the array based vector
+        T[] array = new T[DEFAULT_LENGTH];
+
+        public ArrayBasedVector() { }
+
+        /// <summary>
+        /// The number of elements currently stored in the ArrayBasedVector
+        /// </summary>
+        // private int count = 0;
+
+        /// <summary>
+        /// The number of elements currently stored in the ArrayBasedVector
+        /// </summary>
+        /// <remarks>
+        /// Notice: This is NOT equal to the length of the array! We can have an array with empty spaces
+        /// that can be filled later
+        /// </remarks>
+        public int Count
+        {
+            get;
+            private set;
+        } = 0;
+
+        /// <summary>
+        /// The get allows you to read at rank (GetElementAtRank)
+        /// The set is a ReplaceAtRank
+        /// </summary>
+        /// <param name="rank"></param>
+        /// <returns></returns>
+        public T this[int rank]
+        {
+            get
+            {
+                if (rank < 0 || rank >= Count)
+                {
+                    // rank cannot be < 0
+                    // rank < count, because we want to refer to an existing element!
+                    throw new ArgumentOutOfRangeException(nameof(rank), $"Rank greater than or equal to 0 and smaller than or equal to {Count-1}, inclusive!");
+                }
+
+                return array[rank];
+            }
+            set
+            {
+                if (rank < 0 || rank >= Count)
+                {
+                    // rank cannot be < 0
+                    // rank < count, because we want to refer to an existing element!
+                    throw new ArgumentOutOfRangeException(nameof(rank), $"Rank greater than or equal to 0 and smaller than or equal to {Count - 1}, inclusive!");
+                }
+
+                array[rank] = value;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="item"></param>
+        /// <remarks>
+        /// Equivalent to InsertAtRank(item, count)
+        /// </remarks>
+        public void Append(T item)
+        {
+            EnsureCapacity();
+
+            // assuming that there is enough space
+            // place the item at the end
+            array[Count] = item;
+            // increment the counter
+            Count++;
+        }
+
+        public void InsertAtRank(T item, int rank)
+        {
+            EnsureCapacity();
+
+            if (rank < 0 || rank > Count)
+            {
+                // rank cannot be < 0
+                // rank <= count, because we do not want any gaps
+                throw new ArgumentOutOfRangeException(nameof(rank), $"Rank must be between 0 and {Count}, inclusive!");
+                
+                // throw new ArgumentOutOfRangeException("rank", );
+            }
+
+            // copy all elements, up to rank, one step forward
+            // starting from the last element, down to the rank parameter
+            // the last element is at index count - 1.
+            for (int i = Count-1; i >= rank; i--)
+            {
+                // copy the element one step forward
+                array[i+1] = array[i];
+            }
+            // You can also use the array COPY method for the above, however, it still takes a lot of time
+            // Here we are making the work requirement for the copy explicit
+
+            // array at rank is now free
+            array[rank] = item;
+            Count++;
+        }
+        
+        private void EnsureCapacity()
+        {
+            if (Count == array.Length)
+            {
+                // Your array is full!
+                // You need to do something about this!
+                throw new NotImplementedException("TODO!");
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("[");
+
+            for (int i = 0; i < Count; i++)
+            {
+                // Append the value found or NULL
+                if (array[i] == null)
+                {
+                    sb.Append("NULL");
+                }
+                else
+                {
+                    sb.Append(array[i]!.ToString());
+                }
+                
+                sb.Append(", "); // you might want to avoid adding a comma at the end of the list, i.e. when i == count - 1
+            }
+
+            sb.Append("]");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ ArrayGrowth/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ArrayGrowth;
+
+Console.WriteLine("Hello, World!");
+
+ArrayBasedVector<int> vector = new ArrayBasedVector<int>();
+
+vector.Append(100);
+vector.Append(200);
+vector.Append(300);
+vector.Append(400);
+vector.Append(500);
+vector.Append(600);
+
+Console.WriteLine(vector[3]);
+vector[4] = 555;
+Console.WriteLine(vector[4]);
+
+Console.WriteLine(vector);
