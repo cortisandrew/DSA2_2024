ac56cc6bc280c9775436ff26bd9d9bdef98d5783
Andrew Cortis <Andrew.Cortis@mcast.edu.mt>
2024-11-25 10:30:27 +0100

    Added new project.

---
+++ HashtableProject/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Dictionary<string, int> studentGrade = new Dictionary<string, int>();
+
+studentGrade.Add("Albert", 80);
+studentGrade.Add("Bernard", 60);
+
+
+// the number of available spaces in my hashtable
+int arrayLength = 10;
+
+// studentName is the key
+string studentName = "Albert";
+int arrayPosition = Math.Abs(studentName.GetHashCode()) % arrayLength;
+
+// the value 80, gets placed in array position
+
+Console.ReadLine();
